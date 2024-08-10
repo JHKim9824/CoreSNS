530d6277f682a7b8d4bdd5b27379aac83a6b5cc8
JHKim9824 <jeonghunkim98@gmail.com>
2024-08-10 19:07:00 +0900

    feat: user register/login/auth

---
+++ src/main/java/com/jhkim9824/coresns/service/UserService.java
@@ -0,0 +1,29 @@
+package com.jhkim9824.coresns.service;
+
+import com.jhkim9824.coresns.entity.User;
+import com.jhkim9824.coresns.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+    }
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
